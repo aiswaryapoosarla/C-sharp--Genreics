c7bfa7439f724f82c236449aabf10d3d402fa839
aiswaryapoosarla <114751281+aiswaryapoosarla@users.noreply.github.com>
2023-12-11 20:57:19 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Generic_class<T>
+    {
+       private List<T> myCollection;
+
+        public Generic_conatiner()
+        {
+            myCollection = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            myCollection.Add(item);
+        }
+
+        public T GetItem(int index)
+        {
+
+            return myCollection[index];
+        }
+    }
+}
